995e9a93bf59e3e7f96eacc44a7cf0d7d98ab24a
ALMA_DEV914 <aungonalna58@gamil.com>
2022-12-14 14:22:54 -0800

    add boostrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import * as serviceWorker from './serviceWorker';
+import './assets/css/bootstrap.min.css'
+import './assets/css/agency.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,5 +12,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+serviceWorker.unregister();
 
