0bab0cdffab42c8e063ee64190b3e72e200abeb8
ALMA_DEV914 <aungonalna58@gamil.com>
2023-01-24 19:23:24 -0800

    update layout plans

---
+++ src/components/campground/CampgroundList.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function CampgroundList() {
+  const [user, setUser] = useState([]);
+
+   const fetchData = () => {
+      return axios.get("")
+      .then ((response) => setUser(response.data))
+   }
+
+   useEffect(() => {
+    fetchData()
+   }, []);
+
+  return (
+    <div>
+       <main>
+      <h1>User List</h1>
+      <ul>
+        {user && user.length > 0 && user.map((userObj, index) => (
+            <li key={userObj.id}>{userObj.name}</li>
+          ))}
+      </ul>
+    </main>
+    </div>
+  )
+}
+
+export default CampgroundList
